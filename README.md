0630157b9d70e3652ea700db0bd9e082ee09c822
Vinay-Krishna15 <74097765+Vinay-Krishna15@users.noreply.github.com>
2020-12-15 23:44:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Supply-Mission-project-23-
-If you press the down arrow the package will fall in the red box.
+# SupplyMission
---
+++ box(side).js
@@ -0,0 +1,21 @@
+class BoxSide {
+    constructor(x, y, width, height,) {
+     
+    this.body = Bodies.rectangle(x, y, width, height,);
+    this.width = width   
+    this.height = height 
+        World.add(world, this.body);
+   // this.body = bodies.rectangle(750,200,20,200);
+    }
+    display(){
+        var pos =this.body.position;
+        //var angle=this.body.angle;
+        push()
+        translate(pos.x,pos.y)
+        //rotate(angle)
+        rectMode(CENTER);
+        fill("red");
+        rect(0,0, this.width, this.height);
+        pop();
+}
+}
